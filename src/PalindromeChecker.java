81d369614156c8110bc0aefc5017d480b0cd8689
samiksha190206 <vv4976@srmist.edu.in>
2026-02-25 10:22:13 +0530

    UC3:String Reverse

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,14 +1,12 @@
 public class PalindromeChecker {
     public static void main(String[] args) {
-        String input = "madam";
-        boolean isPalindrome = true;
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        System.out.println("Input text : " + input);
-        System.out.println("Is it a palindrome? " + isPalindrome);
+                String input = "madam";
+                String reverse= "";
+                for (int i = input.length() - 1; i >= 0; i--) {
+                    reverse += input.charAt(i);
+                }
+                boolean isPalindrome = input.equals(reverse);
+                System.out.println("Input text : " + input);
+                System.out.println("Is it a palindrome? " + isPalindrome);
             }
         }
