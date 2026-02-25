13a67a395c9a89113053232306b7daf4c446107d
samiksha190206 <vv4976@srmist.edu.in>
2026-02-25 10:40:23 +0530

    UC5:Stack Palindrome

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,19 +1,21 @@
+import java.util.Stack;
 public class PalindromeChecker {
     public static void main(String[] args) {
-        String input = "radar";
-        char[] chars = input.toCharArray();
-        int start = 0;
-        int end = chars.length - 1;
-        boolean isPalindrome = true;
-        while (start < end) {
-            if (chars[start] != chars[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
+        String input = "noon";
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+                }
+
+                boolean isPalindrome = true;
+                for (char c : input.toCharArray()) {
+                    if (c != stack.pop()) {
+                        isPalindrome = false;
+                        break;
+                    }
                 }
                 System.out.println("Input text : " + input);
                 System.out.println("Is it a palindrome? " + isPalindrome);
             }
         }
+
